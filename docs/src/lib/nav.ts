71f8f020de9dff2bd767c647a7c26265e703422c
Sayan Deep Saha <ssaha@connve.com>
2026-06-03 14:41:38 +0200

    feat: add mongo documentation

--- docs/src/lib/nav.ts
+++ docs/src/lib/nav.ts
@@ -101,6 +101,14 @@ export const navigation: NavSection[] = [
 		]
 	},
 	{
+		title: 'MongoDB',
+		icon: '/icons/mongodb.svg',
+		items: [
+			{ title: 'Reader', href: '/mongo/reader' },
+			{ title: 'Writer', href: '/mongo/writer' }
+		]
+	},
+	{
 		title: 'MSSQL',
 		icon: '/icons/mssql.svg',
 		items: [
